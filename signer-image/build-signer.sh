ef49a7c1d342ec08ef6aca7c4d805b3f0e4b0cb2
Bence Csati <bence.csati@axoflow.com>
2025-11-03 13:05:26 +0100

    chore(signer): use fixed tags
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- signer-image/build-signer.sh
+++ signer-image/build-signer.sh
@@ -10,17 +10,12 @@ set -euo pipefail
 #################################################
 # Configuration
 #################################################
-IMAGE_REPO=${DOCKER_IMAGE_REPO:-axoflow/signer}
-BASE_IMAGE=${DOCKER_BASE_IMAGE:-debian:stable-slim}
-OSSLSIGNCODE_VERSION=${OSSLSIGNCODE_VERSION:-latest}
-DOCKERFILE=${DOCKERFILE:-signer-image/Dockerfile}
+TAG="${1:-latest}"
 
-# Generate image tag
-if [[ $OSSLSIGNCODE_VERSION == "latest" ]]; then
-    TAG="latest"
-else
-    TAG="$OSSLSIGNCODE_VERSION"
-fi
+IMAGE_REPO=${DOCKER_IMAGE_REPO:-axoflow/signer}
+BASE_IMAGE=${DOCKER_BASE_IMAGE:-debian}
+BASE_IMAGE_VERSION=${DOCKER_BASE_IMAGE_VERSION:-stable-slim@sha256:a771c85b2287eae7ce8fe0a4c2637d575c5d991555ae680c187c5572153648d9}
+DOCKERFILE=${DOCKERFILE:-signer-image/Dockerfile}
 
 IMAGE_NAME="$IMAGE_REPO:$TAG"
 
@@ -34,7 +29,7 @@ echo "osslsigncode version: $OSSLSIGNCODE_VERSION"
 docker build \
     --file "$DOCKERFILE" \
     --build-arg BASE_IMAGE="$BASE_IMAGE" \
-    --build-arg OSSLSIGNCODE_VERSION="$OSSLSIGNCODE_VERSION" \
+    --build-arg BASE_IMAGE_VERSION="$BASE_IMAGE_VERSION" \
     --tag "$IMAGE_NAME" \
     .
 
