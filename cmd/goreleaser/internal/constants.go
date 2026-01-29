bc58312b64cc2ff897a7f6befb4b7858a8b672bd
Bence Csati <bence.csati@axoflow.com>
2026-01-29 15:22:47 +0100

    feat: go updates
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

---
+++ cmd/goreleaser/internal/constants.go
@@ -0,0 +1,13 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package internal
+
+const (
+	axoflowOtelCollector = "axoflow-otel-collector"
+	binaryNamePrefix     = "otelcol"
+	imageNamePrefix      = "axoflow-otel-collector"
+
+	projectName     = "axoflow-otel-collector-releases"
+	defaultBuildDir = "_build"
+)
